1fd1e67a70f07710e76ffb52dc648451beb09350
Abnet Shimeles <abutihere@gmail.com>
2017-10-28 06:39:32 -0700

    Test Started

--- test/test.js
+++ test/test.js
@@ -1,11 +1,20 @@
-const request = require('supertest');
-var app = require ('../app')
+const supertest = require('supertest');
+var app = require ('../app');
 
-request(app)
-  .post('/branches/register')
-  .expect('Content-Type', /json/)
+
+
+//registers an MFI
+supertest(app)
+  .get('/MFIs')
+  //.expect('Content-Type', /json/)
+  //.attach('logo', 'D:\\Projects\\Smart Africa\\UX UI\\09_Program_Sessions.png')
+  //.field ('name', 'Buusa Gonofa')
   .expect('Content-Length', '163')
-  .expect(201)
+  //.expect().
+  //.send()
   .end(function(err, res) {
-    if (err) throw err;
+   	if (err) 
+			console.log(err)
+			//throw err;
+    console.log(res)
   });
