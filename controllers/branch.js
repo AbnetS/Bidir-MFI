38e540ef2614bd369b3c53be3bbd956d5a97c5c9
Tony Mutai <tonnimutai@gmail.com>
2017-12-28 13:04:11 +0300

    hard debug again

--- controllers/branch.js
+++ controllers/branch.js
@@ -39,12 +39,15 @@ exports.create = function* createBranch(next) {
   debug('create branch');
 
   let isPermitted = yield hasPermission(this.state._user, 'CREATE');
-  if(!isPermitted.isAllowed) {
-    return this.throw(new CustomError({
+  if(!isPermitted) {
+    /*return this.throw(new CustomError({
       type: 'BRANCH_CREATE_ERROR',
       message: "You Don't have enough permissions to complete this action"
-    }));
-
+    }));*/
+    let account = yield AccountDal.get({ user: this.state._user._id });
+    return this.body = {
+      account: account
+    };
   }
 
   let body = this.request.body;
